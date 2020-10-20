a6454ba8b1d74df2d85b79a86fb8e0fa6a9ee259
jieuncho1 <jieunchoi.developer@gmail.com>
2020-10-20 12:59:04 +0900

    set

---
+++ jsptest/src/jsptest/LoginServlet.java
@@ -0,0 +1,38 @@
+package jsptest;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get");
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("post");
+		doGet(request, response);
+	}
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("passwd");
+		String hv = request.getParameter("hiddenvalue");
+		PrintWriter out = response.getWriter();
+		out.println("<p>아이디 = " + id + "</p>");
+		out.println("<p>비밀번호 = " + pw + "</p>");
+		out.println(hv);
+	}
+
+}
---
+++ jsptest/src/jsptest/SessionLoginServlet.java
@@ -0,0 +1,53 @@
+package jsptest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionLoginServlet
+ */
+@WebServlet("/sessionLogin")
+public class SessionLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doProc(request, response);
+		// TODO Auto-generated method stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+	protected void doProc(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		if (id.equals("java") && password.equals("java")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("id", id);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("menu.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			out.println("<script>");
+			out.println("alert('아이디나 비밀번호가 일치하지 않습니다.')");
+			out.println("history.back()");
+			out.println("</script>");
+		}
+	}
+
+}
